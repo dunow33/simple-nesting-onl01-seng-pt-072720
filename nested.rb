acd39c2b675cb2cb3f65be1d17ca62225fcf10d4
Brian Donnoe <dunow33@hotmail.com>
2020-08-07 02:02:42 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -84,7 +84,7 @@ def adding_matz
         }
      }
      
-     programmer_hash.shift = "yukihiro_matsumoto"
+     programmer_hash[0] = "yukihiro_matsumoto"
 
     #programmer_hash[0][:yukihiro_matsumoto] =
      #     { 
