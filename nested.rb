c5902acc9639d6b48ccf168406dca8fd1b97fa01
Brian Donnoe <dunow33@hotmail.com>
2020-08-07 01:55:51 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -84,9 +84,9 @@ def adding_matz
         }
      }
      
-     programmer_hash[:yukihiro_matsumoto][0]
+     programmer_hash[0] = "yukihiro_matsumoto"
 
-    programmer_hash[:yukihiro_matsumoto][0] = 
+    programmer_hash[:yukihiro_matsumoto] = 
           { 
             {:known_for => "Ruby"},
             {:languages => ["LISP", "C"]}
